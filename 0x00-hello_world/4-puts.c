71de9084ba5cab216b892de49aeac727b2d321fd
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-13 16:41:39 -0700

    add 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints line using puts function
  * Return : 0 (Sucess)
  */
-int main(void
+int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
 return (0);
