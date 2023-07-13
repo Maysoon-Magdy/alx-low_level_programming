f63d368054df1de3749f27098b6afd706b2303f2
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-13 16:59:36 -0700

    add printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints line using prinf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
