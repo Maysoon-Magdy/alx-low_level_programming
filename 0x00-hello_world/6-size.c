c09596997ef20c788e224952f1a1ad51df1d11ce
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-13 17:36:49 -0700

    add size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int b;
 long c;
 long long int d;
 float f;
-printf("Size of a char:" + sizeof(a) + "byte(s)\n");
-printf("Size of a char:" + sizeof(b) + "byte(s)\n");
-printf("Size of a char:" + sizeof(c) + "byte(s)\n");
-printf("Size of a char:" + sizeof(d) + "byte(s)\n");
-printf("Size of a char:" + sizeof(f) + "byte(s)\n");
+printf("Size of a char: %lu byte(s)\n", (unsigned char) sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned int) sizeof(b));
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long long int) sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
