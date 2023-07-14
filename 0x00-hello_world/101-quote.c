ccafb9ad401950a11bfe6c3bd0fc40266c1022c2
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-13 18:34:01 -0700

    add fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<unistd.h>
 /**
  * main - A program that prints line without printf or put
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-,strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return (1);
 }
+
