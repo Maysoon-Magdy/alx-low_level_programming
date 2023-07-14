727eb5294a761930f34cd325942594aa2f44b41a
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-13 17:27:20 -0700

    add size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Aprogram that prints the size of various types on the
+ * computer it is compiled and run on
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long c;
+long long int d;
+float f;
+printf("Size of a char:" + sizeof(a) + "byte(s)\n");
+printf("Size of a char:" + sizeof(b) + "byte(s)\n");
+printf("Size of a char:" + sizeof(c) + "byte(s)\n");
+printf("Size of a char:" + sizeof(d) + "byte(s)\n");
+printf("Size of a char:" + sizeof(f) + "byte(s)\n");
+return (0);
+}
