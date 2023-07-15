1f2fd0fea697f48779cd8c8b9c08d74aa3b7da99
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-14 19:17:38 -0700

    added fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that Complete the source code in order to
+ * print the last digit of the number stored in the variable n.
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
