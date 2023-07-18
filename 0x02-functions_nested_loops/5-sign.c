72c6cf173b336df3d85fcc510a37618f9b029f3b
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-18 16:41:57 -0700

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - A program that checks the operator
+ *
+ * @n : intiger character in ASCII
+ *
+ * Return: 1 if lower , 0 if not  (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	_putchar('\n');
+}
+
