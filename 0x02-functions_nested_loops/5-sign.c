3dc17473309fbf9bf2223946b3b0a532178d289d
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-18 16:50:03 -0700

    add 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,14 +17,13 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar(45);
-		return (0);
+		return (-1);
 	}
-	else if (n == 0)
+	else
 	{
 		_putchar(48);
 		return (0);
 	}
 	_putchar('\n');
-	return (0);
 }
 
