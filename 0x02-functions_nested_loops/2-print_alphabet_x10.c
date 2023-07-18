761be72ae064fc0221c8a916763ab3da4b2e9b3b
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-18 15:33:12 -0700

    add 2-alphabet*10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - A program that prints 10 times
+ * the alphabits with line using prinf function
+ *
+ * Return: Always 0 (Success)
+ */
+oid print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		print_alphabet();
+	}
+}
