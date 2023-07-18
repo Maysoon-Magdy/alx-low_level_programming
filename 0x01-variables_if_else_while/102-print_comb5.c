156c8d5cb6d9a190914c16354cf08ed8a56aad5d
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-17 18:58:50 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - A program that Write a program that prints all
+ * possible combinations of two two-digit numbers n.
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9' ; c++)
+{
+for (d = '0' ; d <= '9' ; d++)
+{
+if ((a == c && b == d) || (a == c && b > d))
+continue;
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
