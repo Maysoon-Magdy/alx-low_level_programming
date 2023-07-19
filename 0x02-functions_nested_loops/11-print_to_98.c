d6941920d182e3cd864ec47830d5662c707d263c
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-18 19:53:06 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,42 @@
 #include<stdio.h>
 /**
- * main - check the code
+ * print_to_98- check the code
  *
- * Return: Always 0.
- */
-int main(void)
+ * @n : integer input
+ *
+ * */
+void print_to_98(int n)
 {
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf('\n');
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf('\n');
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
 }
