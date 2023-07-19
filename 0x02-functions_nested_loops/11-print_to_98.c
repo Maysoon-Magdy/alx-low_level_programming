4f4b55547b1658d09a407b4bca62c2e27dc20730
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-18 20:03:02 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
-				printf('\n');
+				putchar('\n');
 				break;
 			}
 			else
@@ -30,7 +30,7 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
-				printf('\n');
+				putchar('\n');
 				break;
 			}
 			else
