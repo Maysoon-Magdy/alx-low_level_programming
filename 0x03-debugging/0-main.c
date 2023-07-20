ad7cf0ebb9ac8190322d772259ee6f1d97dbbaf4
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-20 00:57:08 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
  * main - Test function positive or negative integer
  * Return: Always 0
- */
+*/
+
 int main(void)
 {
-	int x;
+	int i;
 
-	x = 0;
-	positive_or_negative(x);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
