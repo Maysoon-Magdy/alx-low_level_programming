87a66501a44da914696cec57341ca57a6441f85e
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-20 17:53:13 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (y = 0; y < 10; y++)
+	for (y = 1; y <= 10; y++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			if (x >= 10)
-				_putchar(49);
-				_putchar(x % 10 + 48);
+				_putchar('1');
+			_putchar(x % 10 + '0');
 		}
 	 _putchar('\n');
 	}
