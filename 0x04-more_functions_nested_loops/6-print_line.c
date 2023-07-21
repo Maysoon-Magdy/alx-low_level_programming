de45a7d4771985c90f9049542eff452e517e5be1
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-20 18:07:30 -0700

    6-printline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a straight line
+ * @n: integer represents line length
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+}
