5c2d1d60c654f730aceaca69d5354584b47327e9
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-24 19:13:27 -0700

    -fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,18 +5,18 @@
  * @s: the string we eant to reverse
  */
 void rev_string(char *s)
-{    
-int i , x , y = 0;
+{
+int i, x, y = 0;
 char temp;
+
 while (s[i])
 i++;
-y = i / 2 ;
+y = i / 2;
 while (y--)
-{ 
+{
 temp = s[i - x - 1];
 s[i - x - 1] = s[x];
-s[x] = temp ;
+s[x] = temp;
 x++;
-}       
-printf("\n");
+}
 }
