3d3ca4597ee5362dd7238586873463bf3d75d527
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-27 16:01:06 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper -  a function.
+ *
+ * @n: string
+ * return: string
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 97 && n <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
