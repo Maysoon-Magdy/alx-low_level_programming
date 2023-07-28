c7b847b454c199828a152d1fff85957284b1feec
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-27 17:33:20 -0700

    Expect The Best. Prepare For The Worst. Capitalize On What Comes. Hello World! Hello-world 0123456hello World Hello World.Hello World Expect The Best. Prepare For The Worst. Capitalize On What Comes. Hello World! Hello-world 0123456hello World Hello World.Hello World

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - A function that capitalizes all words
+ * @n: the string
+ * Return: n as string after doing the capi.
+ */
+
+char *cap_string(char *n)
+{
+int i;
+
+i = 0;
+while (n[i])
+{
+while (!(n[i] >= 'a' && n[i] <= 'z'))
+i++;
+if (n[i - 1] == ' '
+|| n[i - 1] == '\t'
+|| n[i - 1] == '\n'
+|| n[i - 1] == ','
+|| n[i - 1] == ';'
+|| n[i - 1] == '.'
+|| n[i - 1] == '!'
+|| n[i - 1] == '?'
+|| n[i - 1] == '"'
+|| n[i - 1] == '('
+|| n[i - 1] == ')'
+|| n[i - 1] == '{'
+|| n[i - 1] == '}'
+|| n[i - 1] == '0')
+
+n[i] -= 32;
+i++;
+}
+return (n);
+}
