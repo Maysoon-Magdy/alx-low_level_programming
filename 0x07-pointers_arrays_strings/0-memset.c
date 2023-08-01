5ecc380fcbe785fe514b8989c8f6e6cd42b7824c
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-31 17:47:20 -0700

    fix betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,14 @@
  * @s: parameter
  * @b: parameter
  * @n: parameter
- * return- string
+ *
+ * Return: Changed
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
+
 	for (i = 0; n > 0; i++)
 	{
 		s[i] = b;
