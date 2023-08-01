a8b64e1ac26c29bd62deb7c6d4a466c6c60c99cb
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-31 18:41:33 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,9 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i, y;
+	int i;
 
-	i = 0;
-	while (s[i])
-		i++;
-
-	for (y = 0; y < i; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
