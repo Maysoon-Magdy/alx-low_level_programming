3cba9da1f2fd847badc31e2e172327148b5819f9
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-31 19:15:27 -0700

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
+	unsigned int x = 0;
 
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-				return (i);
+			{
+				x++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (x);
 		}
+
 	s++;
 	}
-	return ('\0');
+	return (x);
 }
