160b659b83b7580537d0398e58ee2d913f0533cb
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-07-31 19:52:18 -0700

    void print_diagsums(int *a, int size)

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagsums - function
+ * @a: p1
+ * @size: p2
+ */
+void print_diagsums(int *a, int size)
+{
+	int num1, num2, i;
+
+	num1 = 0;
+	num2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		num1 = num1 + a[i * size + i];
+	}
+
+	for (i = size - 1; i >= 0; y--)
+	{
+		num2 += a[i * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", num1, num2);
+}
