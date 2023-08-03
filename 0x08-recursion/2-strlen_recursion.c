f8f51c37db77659cff4ecbd22a6e08b8f2a6c528
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-02 17:09:03 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,11 +11,9 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		while (s[i] != '\0')
-		{
-			i++;
-			_strlen_recursion(s - 1);
-		}
+		i++;
+		i += _strlen_recursion(s - 1);
 	}
+
 	return (i);
 }
