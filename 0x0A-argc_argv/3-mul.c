feb1e32e5f0d314dae58d5ba89fa563e07fa7476
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-04 18:33:40 -0700

    push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _atoi - a function that convert a string to an integer.
+ * @s:the string wanted to be converted.
+ * Return: the converted integer.
+ */
+int _atoi(char *s)
+{
+        unsigned int number = 0;
+        int sign = 1;
+
+        do {
+                if (*s == '-')
+                        sign = sign * -1;
+                else if (*s >= '0' && *s <= '9')
+                        number = (number * 10) + (*s - '0');
+                else if (number > 0)
+                        break;
+        } while (*s++);
+        return (number * sign);
+}
+/**
  * main -  a program that prints its name, followed by a new line.
  * @argc: num of inputs
  * @argv:array of charachters
