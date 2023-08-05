14ab2a77ed9288f571fbc87541c6559895535d06
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-04 17:47:34 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints its name, followed by a new line.
+ * @argc: num of inputs
+ * @argv:array of charachters
+ * Return: always 0 success
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc);
+
+	return (0);
+}
+
