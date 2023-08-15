d2ff9e653a4feaa557d200a07c2e4433dbe78a71
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-15 16:48:00 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,18 @@
-#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
 
