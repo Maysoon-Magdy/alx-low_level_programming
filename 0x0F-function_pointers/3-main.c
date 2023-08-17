e67390008fe28f807be3cb49811c893284258129
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-17 14:39:38 -0700

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
  *
  * Return: 0
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -28,17 +28,17 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0)) ||
-	((*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	printf("%d\n", get_op_fun(op)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 	
 	return (0);
 }
