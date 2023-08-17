4bbc182005cdc4db983eeba097f9f526a8a81db3
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-16 20:16:16 -0700

    last

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - Return the sum
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sum
+ *
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Return the sub
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sub
+ *
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Return the mul
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: mul
+ *
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Return the div
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: div
+ *
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Return the mod
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: mod
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
