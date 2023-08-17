6db9bec988ed16772e947ae117029f1fdfe1863e
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-17 13:54:20 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
         for (i = 0; i < n; i++)
        {
+	       if (str == NULL)
+		       printf("(nil)");
+	       else
                 printf("%s", va_arg(str, char *));
 
                 if (i != (n - 1) && separator != NULL)
