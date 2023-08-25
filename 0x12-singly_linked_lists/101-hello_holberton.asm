0c0e8a55f1ec477ccbd9fccf5f7ec103fd67766d
Maysoon-Magdy <Maysoon.Magdy@icloud.com>
2023-08-24 19:00:54 -0700

    make change

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,10 +3,10 @@ section		.text
 	global	main
 
 main:
-	mov	edi, msg
+	mov	edi, myMsg
 	mov	eax, 0
 	call	printf
 
 
 section		.data
-	msg db 'Hello, Holberton', 0xa, 0
+	myMsg db 'Hello, Holberton', 0xa, 0
