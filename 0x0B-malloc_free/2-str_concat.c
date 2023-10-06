40cf4c88340aedfef6c1f654c306038b062caefa
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-05 17:03:21 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2, i, size = 0;
 	char *s;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 
 	for (; s1[size1] != '\0'; size1++)
 		;
