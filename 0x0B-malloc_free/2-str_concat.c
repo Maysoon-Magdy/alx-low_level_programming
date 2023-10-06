bb4e54219a0049e9633efa80ec89795d104e2670
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-05 17:11:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - a function to get strinf length
+ * @str: string
+ *
+ * Return: string size
+ */
+int _strlen(char *str)
+{
+	int size = 0;
+
+	for (; str[size] != '\0'; size++)
+		;
+	return (size);
+}
+
+/**
  * str_concat - a function that concatenates two strings.
  * @s1: pointer to the first string
  * @s2: pointer to the second string
@@ -12,7 +27,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i, size;
+	int size1, size2, i;
 	char *s;
 
 	if (s1 == NULL)
@@ -20,19 +35,15 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = '\0';
 
-	for (; s1[size1] != '\0'; size1++)
-		;
-	for (; s2[size2] != '\0'; size2++)
-		;
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
 
-	size = size1 + size2;
-
-	s = (char *)malloc(size * sizeof(char) + 1);
+	s = (char *)malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (s == 0)
 		return (NULL);
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i <= (size1 + size2); i++)
 	{
 		if (i < size1)
 			s[i] = s1[i];
