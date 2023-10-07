231cdb4126040266a9839800cc6ac9ac11eccdba
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-07 14:04:43 -0700

    new

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,6 @@
 #include "main.h"
-/**
- * main - the main
- *
- * Return: Always 0
- */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal"
-			" was to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
