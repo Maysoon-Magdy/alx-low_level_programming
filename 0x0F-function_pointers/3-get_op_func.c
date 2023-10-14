03a391066a0967cc88a9cc107e734290ca530384
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-13 17:06:50 -0700

    add 3.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * get_op_func - selects the correct function to perform the operation.
+ * @s: the operator
+ *
+ * Return: Apointer to the selected function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a & b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the sub of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sub of a & b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the mult. of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: mult. of a & b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function div of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: div two numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function modlus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: mod  two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
